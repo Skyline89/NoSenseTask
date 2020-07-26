5ecf51c6f41977971c9ebe60c92fe4294d6748a3
Skyline89 <aleko.mesarkishvili@gmail.com>
2020-07-26 16:01:56 +0400

    modified No Sense

---
+++ No Sense/EnumerExstension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace No_Sense
+{
+    public static class EnumerExstension
+    {
+        public static IEnumerable<TSource> ThisDoesntMakeAnySense<TSource>(this IEnumerable<TSource> list, string[] numbers, Func<TSource,bool> predicate, Func<TSource,TSource> createNewRecord)
+        {
+            foreach (var element in list)
+            {
+                if (predicate(element))
+                    yield return default;
+            }
+
+            foreach (var item in list)
+            {
+                if (predicate(item) == false)
+                    yield return createNewRecord(item);
+            }
+
+            foreach (var number in numbers)
+            {
+                if (number == null)
+                    throw  new ArgumentNullException(nameof(list), "please enter number instead of null");
+            }
+
+            if (numbers == null)
+                throw new ArgumentNullException(nameof(numbers), "It's not valid array");
+
+        }
+    }
+}
--- No Sense/Program.cs
+++ No Sense/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
@@ -9,49 +12,20 @@ using Microsoft.VisualBasic.CompilerServices;
 
 namespace No_Sense
 {
-    public static class EnumerExstension
-    {
-        int
-        public static IEnumerable<T> ThisDoesntMakeAnySense<T>(this IEnumerable<T> list, Func<int, int, bool> checking,
-            Func<T,T> createNewRecord)
-        {
-            foreach (var item in list)
-            {
-                if (item == null)
-                    throw new ArgumentNullException(nameof(item), "Please enter number instead of null");
-            }
-            return default;
-            bool res = checking(list,12)
-        }
-
-        public static bool IsThere(int [] numbers, int element)
-        {
-            foreach (var number in numbers)
-            {
-                if (number == element)
-                    return true;
-            }
-
-            return false;
-        }
-    }
-    class Program
+    public class Program
     {
         public static void Main()
         {
             Console.WriteLine("please enter numbers and divide them with ',':");
             var str = Console.ReadLine().Split(',');
             var numbers = Array.ConvertAll(str,int.Parse);
-            foreach (var number in numbers)
+            var sense = numbers.ThisDoesntMakeAnySense(str, n => n.Equals(5), x => x);
+            foreach (var values in sense)
             {
-                Console.WriteLine(number);
+                Console.WriteLine(values);
             }
-
-        }
-
-        public static bool CheckingArrayElement(int[] array, int element)
-        {
-            return Array.Exists(array, x => x == element);
+            //Func<int, bool> checking = (int num) => num.Equals(23);
+            //Func<int, int> create = (int numb) => numb;
         }
     }
 }
